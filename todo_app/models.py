0a60c3485e444ef1eadfd829cc6eda5f91cf7ced
cemen <frodo.work@gmail.com>
2025-04-14 10:43:04 +0300

    models update: title is required

--- todo_app/models.py
+++ todo_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TodoTask(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=64, null=False, blank=False)
     description = models.TextField(blank=True, null=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
