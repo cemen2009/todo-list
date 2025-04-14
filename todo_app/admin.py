2c97b55ab24755b0fd879a45368a68d418ac869a
cemen <frodo.work@gmail.com>
2025-04-14 10:29:49 +0300

    created admin model for TodoTask model

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_app.models import TodoTask
+
+
+@admin.register(TodoTask)
+class TodoTaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "deadline", "is_completed")
+    list_filter = ("is_completed",)
+    search_fields = ("title",)
+    ordering = ("-deadline", "-created_at")
