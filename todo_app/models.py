bee18ae4b8e7387178359e2de7dd2cacb364b2e0
cemen <frodo.work@gmail.com>
2025-04-14 10:00:15 +0300

    created models with basic attributes + added *.sqlite3 to .gitignore

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class TodoTask(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Task: {self.title}"
+
+    class Meta:
+        ordering = ['-deadline']
